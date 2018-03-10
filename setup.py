24d260fdc568b544ea823d739e4bec3e275a61cb
Nate Vogel <nathan.c.vogel@gmail.com>
2018-03-10 08:15:28 -0500

    updated version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='ls_report_agent',
-      version='0.1',
+      version='0.2',
       description='Agent for collecting a report from the LegalServer report API',
       author='Nate Vogel',
       packages=['ls_report_agent'])
